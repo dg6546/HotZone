76e624843f16c50771120c7f730cae6b131e6e8d
dg6546 <dg6546@gmail.com>
2020-11-02 21:58:18 +0800

    make it better

--- CHP/settings.py
+++ CHP/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gljz!ijg2at)*u2c2q(45v!27a^jrcdfm&5mb(i-4dpah+%639'
+SECRET_KEY = env('HOTZONE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['afternoon-dawn-73910.herokuapp.com', 'localhost', '127.0.0.1']
 
