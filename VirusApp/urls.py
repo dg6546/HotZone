5a93445af7d1a9a0f129bc46d3589298aedb6493
dg6546 <dg6546@gmail.com>
2020-11-20 19:37:00 +0800

    deleted add case and patient

--- VirusApp/urls.py
+++ VirusApp/urls.py
@@ -9,13 +9,12 @@ urlpatterns = [
     #path('location/', search_location_form_view, name='search_location'),
     #path('location/new', New_location_form_view, name='new_location'),
     #path('new_case/', New_case_form_view,  name='new_case' ),
-    path('patient/new', New_patient_form_view,  name='new_patient' ),
+    #path('patient/new', New_patient_form_view,  name='new_patient' ),
     path('search/patient', search_patient_form_view, name='search_patient'),
     path('search/case', search_case_form_view, name='search_case'),
-    path('patient/<int:patient_id>/new_case', New_case_form_view, name='new_case'),
+    #path('patient/<int:patient_id>/new_case', New_case_form_view, name='new_case'),
     path('case_detail/<int:case_id>/new_visit', new_visit_form_view, name='new_visit'),
     path('case_detail/<int:case_id>/new_location', search_location_form_view, name='new_location'),
     path('case_detail/<int:case_id>/result', search_location_form_view, name='search_location_result'),
     path('case_detail/<int:case_id>/add_visit', New_location_form_view, name='add_location'),
-
 ]
